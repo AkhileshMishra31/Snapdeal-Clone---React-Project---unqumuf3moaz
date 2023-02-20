326e0f199e82cc2484b7e13e89197fd59c1b8db1
Harry <akhilesh@gmail.com>
2023-02-20 23:46:26 +0530

    cart and login  design ready

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CartProducts from '../CartProducts/CartProducts'
 import "./Cart.css"
 
 const Cart = () => {
@@ -7,7 +8,18 @@ const Cart = () => {
             <h1 className='cart_heading'>Cart</h1>
             <hr></hr>
             <div className='cart_items'>
-
+                <CartProducts />
+                <CartProducts />
+                <CartProducts />
+            </div>
+            <hr></hr>
+            <div className='subtotal'>
+                <p>Subtotal:-</p>
+                <p>RS.233</p>  
+                <div>
+                    <p>Items</p>
+                    <p>3</p>
+                </div> 
             </div>
         </div>
     )
--- src/components/CartProducts/CartProducts.js
+++ src/components/CartProducts/CartProducts.js
@@ -2,9 +2,44 @@ import React from 'react'
 import "./CartProducts.css"
 
 const CartProducts = () => {
-  return (
-    <div>CartProducts</div>
-  )
+    const inreasequantity = () => {
+
+    }
+
+    const removefrombasket = () => {
+
+    }
+
+    const decreasequantity = () => {
+
+    }
+    return (
+        <div className='cart_products'>
+            <div className='cart_products_image'>
+                <img src="https://n4.sdlcdn.com/imgs/k/e/u/large/Veirdo-100-Cotton-Regular-Fit-SDL302182620-1-f0fac.jpg" />
+            </div>
+            <div className='cart_products_description'>
+                <h2>Rigo - Navy Blue Cotton Regular Fit Women's Jumpsuit ( Pack of 1 )</h2>
+                <div>
+                    <span>⭐</span>
+                    <span>⭐</span>
+                    <span>⭐</span>
+                    <span>⭐</span>
+                </div>
+                <div className='price'>
+                    <span>Rs 299</span>
+                    <span> 75% OFF </span>
+                    <div className='product_quantity'>
+                        <p>Quantity</p>
+                        <button onClick={decreasequantity} className='increse_quantity'>-</button>
+                        <p>2</p>
+                        <button onClick={inreasequantity} className='increse_quantity'>+</button>
+                    </div>
+                </div>
+                <button className='remove_button'>Remove to cart</button>
+            </div>
+        </div>
+    )
 }
 
 export default CartProducts
